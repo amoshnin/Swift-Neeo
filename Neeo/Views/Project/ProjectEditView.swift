94e11cd1014a9f90c2d970a4ac7373d91210572a
Artem <swift.uix@gmail.com>
2021-03-06 13:50:11 +0100

    firestore working

--- Neeo/Views/Project/ProjectEditView.swift
+++ Neeo/Views/Project/ProjectEditView.swift
@@ -20,15 +20,76 @@ enum Action {
 
 struct ProjectEditView: View {
     // MARK: - State
+    @Environment(\.presentationMode) private var presentationMode
+    @State var isActionSheetPresent = false
     
     // MARK: - State (Initialiser-modifiable)
+    @StateObject var viewModel = ProjectViewModel()
+    var mode: Mode = .new
+    var completionHandler: ((Result<Action, Error>) -> Void)?
     
     // MARK: - UI Components
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                Section(header: Text("Project")) {
+                    TextField("Title", text: $viewModel.project.title)
+                    TextField("Description", text: $viewModel.project.description)
+                } //: SECTION
+                
+                if self.mode == .edit {
+                    Section {
+                        Button("Delete book") { self.isActionSheetPresent.toggle() }
+                            .foregroundColor(.red)
+                    } //: SECTION
+                }
+            } //: FORM
+            .navigationTitle(self.mode == .new ? "New book" : viewModel.project.title)
+            .navigationBarTitleDisplayMode(self.mode == .new ? .inline : .large)
+            .navigationBarItems(
+                leading: cancelButton(),
+                trailing: saveButton()
+            )
+            .actionSheet(isPresented: self.$isActionSheetPresent) {
+                ActionSheet(title: Text("Are you sure?"), buttons:
+                                [.destructive(Text("Delete book"), action: { self.handleDeleteTapped() }),
+                                 .cancel()])
+            }
+        } //: NAVIGATION_VIEW
+    }
+    
+    private func cancelButton() -> some View {
+        Button(action: { self.handleCancelTapped() }) {
+            Text("Cancel")
+        }
+    }
+    
+    private func saveButton() -> some View {
+        Button(action: { self.handleDoneTapped() }) {
+            Text(self.mode == .new ? "Done" : "Save")
+        } //: BUTTON
+        .disabled(!viewModel.modified)
     }
     
     // MARK: - Action Handlers
+    private func handleCancelTapped() {
+        self.dismiss()
+    }
+    
+    private func handleDoneTapped() {
+        self.viewModel.handleDoneTapped()
+        self.dismiss()
+    }
+    
+    private func handleDeleteTapped() {
+        viewModel.handleDeleteTapped()
+        self.dismiss()
+        self.completionHandler?(.success(.delete))
+    }
+    
+    private func dismiss() {
+        self.presentationMode.wrappedValue.dismiss()
+    }
 }
 
 struct ProjectEditView_Previews: PreviewProvider {
